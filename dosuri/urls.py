4665fc1298f285ec315bafe6d9c71e11ca0f17fa
jihoon-MRX <jihoon.kim@makinarocks.ai>
2022-10-03 20:43:16 +0900

    feat: simple jwt + swagger

--- dosuri/urls.py
+++ dosuri/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include("rest_framework.urls")),
-    # path('api/rest-auth/', include("rest_auth.urls")),
+
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # user
+    path('api/user/v1/', include('dosuri.user.urls')),
 ]
