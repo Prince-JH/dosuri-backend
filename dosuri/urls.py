6e79f71075d08870e53aa7d1c14f526f540653a8
Prince-JH <alahoon@naver.com>
2022-10-15 03:07:24 +0900

    feat: github action trial #22

--- dosuri/urls.py
+++ dosuri/urls.py
@@ -15,19 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#
-# from drf_spectacular.views import (
-#     SpectacularAPIView,
-#     SpectacularRedocView,
-#     SpectacularSwaggerView,
-# )
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    # path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # user
     path('api/user/v1/', include('dosuri.user.urls')),
