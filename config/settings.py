b06f8b013f6e14ab81b0b90b8525c3f405228ffc
Prince-JH <alahoon@naver.com>
2022-10-20 00:59:37 +0900

    feat: default pagination

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'maf_core.pagination.LinkHeaderPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
