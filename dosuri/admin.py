433c258f7947a3b0d83afb52487c230df4759974
Prince-JH <alahoon@naver.com>
2022-10-21 00:26:10 +0900

    feat: add address on django admin

--- dosuri/admin.py
+++ dosuri/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from dosuri.user.models import User
+from dosuri.hospital import models as hm
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'do', 'city', 'gun', 'gu')
+    readonly_fields = ('uuid', 'created_at')
+
+
+admin.site.register(hm.Address, AddressAdmin)
 
 admin.site.register(User)
