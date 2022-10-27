86944bf40ac767f6a4d486741c372047970e4d88
Prince-JH <alahoon@naver.com>
2022-10-28 02:01:07 +0900

    feat: doctor serializers

--- dosuri/hospital/serializers.py
+++ dosuri/hospital/serializers.py
@@ -10,8 +10,8 @@ class Hospital(s.ModelSerializer):
         queryset=m.Address.objects.all()
     )
     name: s.Field = s.CharField()
-    introduction: s.Field = s.CharField(allow_blank=True)
-    phone_no: s.Field = s.CharField(allow_blank=True)
+    introduction: s.Field = s.CharField(allow_null=True)
+    phone_no: s.Field = s.CharField(allow_null=True)
     up_count: s.Field = s.IntegerField(read_only=True)
     view_count: s.Field = s.IntegerField(read_only=True)
     is_partner: s.Field = s.BooleanField()
@@ -43,15 +43,46 @@ class HospitalCalendar(s.ModelSerializer):
         slug_field='uuid',
         queryset=m.Hospital.objects.all()
     )
-    monday: s.Field = s.CharField(allow_blank=True)
-    tuesday: s.Field = s.CharField(allow_blank=True)
-    wednesday: s.Field = s.CharField(allow_blank=True)
-    thursday: s.Field = s.CharField(allow_blank=True)
-    friday: s.Field = s.CharField(allow_blank=True)
-    saturday: s.Field = s.CharField(allow_blank=True)
-    sunday: s.Field = s.CharField(allow_blank=True)
+    monday: s.Field = s.CharField(allow_null=True)
+    tuesday: s.Field = s.CharField(allow_null=True)
+    wednesday: s.Field = s.CharField(allow_null=True)
+    thursday: s.Field = s.CharField(allow_null=True)
+    friday: s.Field = s.CharField(allow_null=True)
+    saturday: s.Field = s.CharField(allow_null=True)
+    sunday: s.Field = s.CharField(allow_null=True)
     created_at: s.Field = s.DateTimeField(read_only=True)
 
     class Meta:
         model = m.HospitalCalendar
+        exclude = ('id',)
+
+
+class Doctor(s.ModelSerializer):
+    uuid: s.Field = s.CharField(read_only=True)
+    hospital: s.Field = s.SlugRelatedField(
+        slug_field='uuid',
+        queryset=m.Hospital.objects.all()
+    )
+    thumbnail_url: s.Field = s.CharField(allow_null=True)
+    name: s.Field = s.CharField()
+    title: s.Field = s.CharField(allow_null=True)
+    position: s.Field = s.CharField(allow_null=True)
+    created_at: s.Field = s.DateTimeField(read_only=True)
+
+    class Meta:
+        model = m.Doctor
+        exclude = ('id',)
+
+
+class DoctorDescription(s.ModelSerializer):
+    uuid: s.Field = s.CharField(read_only=True)
+    doctor: s.Field = s.SlugRelatedField(
+        slug_field='uuid',
+        queryset=m.Doctor.objects.all()
+    )
+    description: s.Field = s.CharField()
+    created_at: s.Field = s.DateTimeField(read_only=True)
+
+    class Meta:
+        model = m.DoctorDescription
         exclude = ('id',)
