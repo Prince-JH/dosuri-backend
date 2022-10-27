ad109c3a6472e9873fbfd7b1cec69e2d27f8f9ae
Prince-JH <alahoon@naver.com>
2022-10-28 02:01:21 +0900

    feat: doctors

--- dosuri/hospital/urls.py
+++ dosuri/hospital/urls.py
@@ -5,10 +5,14 @@ from dosuri.hospital import views as v
 
 urlpatterns = [
 
-    path('v1/hospitals/', v.HospitalList.as_view(), name='hospital-list'),
+    path('v1/hospitals/', v.HospitalList.as_view(), name='hospital'),
     path('v1/hospitals/<uuid>/', v.HospitalDetail.as_view(), name='hospital-detail'),
     path('v1/hospital-calendars/', v.HospitalCalendarList.as_view(), name='hospital-calendar'),
     path('v1/hospital-calendars/<uuid>/', v.HospitalCalendarDetail.as_view(), name='hospital-calendar-detail'),
     path('v1/hospital-images/', v.HospitalImageList.as_view(), name='hospital-image'),
     path('v1/hospital-images/<uuid>/', v.HospitalImageDetail.as_view(), name='hospital-image-detail'),
+    path('v1/doctors/', v.DoctorList.as_view(), name='doctor'),
+    path('v1/doctors/<uuid>/', v.DoctorDetail.as_view(), name='doctor-detail'),
+    path('v1/doctor-descriptions/', v.DoctorDescriptionList.as_view(), name='doctor-description'),
+    path('v1/doctor-descriptions/<uuid>/', v.DoctorDescriptionDetail.as_view(), name='doctor-description-detail'),
 ]
--- dosuri/hospital/views.py
+++ dosuri/hospital/views.py
@@ -16,9 +16,10 @@ class HospitalList(g.ListCreateAPIView):
     permission_classes = [p.AllowAny]
     queryset = m.Hospital.objects.all()
     serializer_class = s.Hospital
-    filter_backends = [rf.OrderingFilter, f.ForeignUuidFilter]
+    filter_backends = [rf.OrderingFilter, f.ForeignUuidFilter, rf.SearchFilter]
     ordering_field = '__all__'
     ordering = ['view_count']
+    search_fields = ['name']
     uuid_filter_params = ['address']
 
 
@@ -48,7 +49,8 @@ class HospitalImageList(g.ListCreateAPIView):
     permission_classes = [p.AllowAny]
     queryset = m.HospitalImage.objects.all()
     serializer_class = s.HospitalImage
-    filter_backends = [f.ForeignUuidFilter]
+    filter_backends = [rf.OrderingFilter, f.ForeignUuidFilter]
+    ordering_field = '__all__'
     uuid_filter_params = ['hospital']
 
 
@@ -57,3 +59,36 @@ class HospitalImageDetail(g.RetrieveUpdateDestroyAPIView):
     queryset = m.HospitalImage.objects.all()
     serializer_class = s.HospitalImage
     lookup_field = 'uuid'
+
+
+class DoctorList(g.ListCreateAPIView):
+    permission_classes = [p.AllowAny]
+    queryset = m.Doctor.objects.all()
+    serializer_class = s.Doctor
+    filter_backends = [rf.OrderingFilter, f.ForeignUuidFilter]
+    ordering_field = '__all__'
+    uuid_filter_params = ['hospital']
+
+
+class DoctorDetail(g.RetrieveUpdateDestroyAPIView):
+    permission_classes = [p.AllowAny]
+    queryset = m.Doctor.objects.all()
+    serializer_class = s.Doctor
+    lookup_field = 'uuid'
+
+
+class DoctorDescriptionList(g.ListCreateAPIView):
+    permission_classes = [p.AllowAny]
+    queryset = m.DoctorDescription.objects.all()
+    serializer_class = s.DoctorDescription
+    filter_backends = [rf.OrderingFilter, f.ForeignUuidFilter]
+    ordering_field = '__all__'
+    uuid_filter_params = ['doctor']
+
+
+class DoctorDescriptionDetail(g.RetrieveUpdateDestroyAPIView):
+    permission_classes = [p.AllowAny]
+    queryset = m.DoctorDescription.objects.all()
+    serializer_class = s.DoctorDescription
+    lookup_field = 'uuid'
+
