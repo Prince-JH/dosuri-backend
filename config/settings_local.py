40cdb3557b8ec29234558eb1eca7d219b0989a2d
Prince-JH <alahoon@naver.com>
2022-11-04 00:45:31 +0900

    feat: local setting with SQL logging

---
+++ config/settings_local.py
@@ -0,0 +1,25 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
