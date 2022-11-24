2292fc783dac45ec410f0addb883deec4270306f
SCHeo <tjheo@ajou.ac.kr>
2022-11-24 23:59:28 +0900

    commmit for deploy, no change

--- dosuri/urls.py
+++ dosuri/urls.py
@@ -24,6 +24,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
