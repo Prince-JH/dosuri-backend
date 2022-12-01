31d14310d0df2f44b34f6087cee4f68da3d35672
Prince-JH <alahoon@naver.com>
2022-12-02 01:58:38 +0900

    feat: resize image and notify to server

--- s3-lambda/src/app.py
+++ s3-lambda/src/app.py
@@ -1,28 +1,53 @@
-import boto3
+from io import BytesIO
+
 import os
-import sys
-import uuid
-from urllib.parse import unquote_plus
-from PIL import Image
+import requests
 
 import json
 import boto3
 
+from urllib.parse import unquote_plus, quote_plus
+from PIL import Image
 
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
-def resize_image(image, resized_path):
-    with Image.open(image) as image:
-        image.resize((480, 480))
-        image.save(resized_path)
+
+def resize_image(image, key):
+    in_mem_file = BytesIO()
+    with Image.open(BytesIO(image)) as im:
+        im.thumbnail((480, 480))
+        im.save(in_mem_file, format=im.format)
+        in_mem_file.seek(0)
+        s3_client.put_object(
+            Body=in_mem_file,
+            Bucket=os.environ.get('SOURCE_BUCKET_NAME') + '-resized',
+            Key=key
+        )
 
 
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket = s3.Bucket(os.environ.get('SOURCE_BUCKET_NAME'))
         key = unquote_plus(record['s3']['object']['key'])
-        image = bucket.Object(key).get()
-        upload_path = key
-        resize_image(image, upload_path)
-        s3_client.upload_file(upload_path, os.environ.get('SOURCE_BUCKET_NAME') + '-resized')
+        image = bucket.Object(key).get()['Body'].read()
+
+        values = key.split('/')
+        domain = values[0]
+        uuid = values[1]
+        path = 'https://dosuri-image.s3.ap-northeast-2.amazonaws.com/' + quote_plus(key)
+        resize_image(image, key)
+        notify_job_finished(domain, uuid, path)
+
+
+def notify_job_finished(domain, uuid, path):
+    url = f"{os.environ.get('DOSURI_URL', 'https://server.dosuri.site')}/hospital/v1/keywords/"
+    data = {
+        'name': path,
+        'is_custom': True,
+        'domain': domain
+    }
+    header = {
+        'Content-Type': 'application/json; charset=utf-8',
+    }
+    res = requests.post(url, headers=header, data=json.dumps(data))
