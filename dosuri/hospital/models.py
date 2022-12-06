1ed6acba89dad4da82beb29ee5784418310f33fa
Prince-JH <alahoon@naver.com>
2022-12-07 00:05:04 +0900

    feat: add hospital latitude, longtitude

--- dosuri/hospital/models.py
+++ dosuri/hospital/models.py
@@ -20,6 +20,8 @@ class Hospital(models.Model):
     view_count = models.IntegerField(default=0)
     is_partner = models.BooleanField(default=False)
     opened_at = models.DateTimeField(null=True)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
