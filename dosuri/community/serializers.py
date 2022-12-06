c2718a7453e6c14de3c7f53cba3ec86e33bb5f71
SCHeo <tjheo@ajou.ac.kr>
2022-12-06 11:04:51 +0900

    modified serializer

--- dosuri/community/serializers.py
+++ dosuri/community/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers as s
 import dosuri.common.models
 from dosuri.community import models as comm
 from dosuri.common import models as cm
+from dosuri.hospital import models as hm
 
 
 class Article(s.ModelSerializer):
@@ -19,13 +20,14 @@ class Article(s.ModelSerializer):
 
 class ArticleAttach(s.ModelSerializer):
     uuid: s.Field = s.CharField(read_only=True)
-    article_auth_id: s.Field = s.SlugRelatedField(
+    article: s.Field = s.SlugRelatedField(
         slug_field='uuid',
-        queryset=comm.ArticleAuth.objects.all()
+        queryset=comm.Article.objects.all()
     )
-    path: s.Field = s.CharField(read_only=True)
+    path: s.Field = s.CharField()
     created_at: s.Field = s.DateTimeField(read_only=True)
 
     class Meta:
         model = dosuri.community.models.ArticleAttach
         exclude = ('id',)
+
