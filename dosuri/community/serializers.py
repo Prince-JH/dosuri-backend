22e6e929afd57dabf4fede61045ec38bc18d617e
SCHeo <tjheo@ajou.ac.kr>
2022-12-07 22:33:51 +0900

    modified user_id to user_uuid

--- dosuri/community/serializers.py
+++ dosuri/community/serializers.py
@@ -4,11 +4,14 @@ import dosuri.common.models
 from dosuri.community import models as comm
 from dosuri.common import models as cm
 from dosuri.hospital import models as hm
-
+from dosuri.user import models as um
 
 class Article(s.ModelSerializer):
     uuid: s.Field = s.CharField(read_only=True)
-    user_id: s.Field = s.IntegerField(read_only=True)
+    user_uuid: s.Field = s.SlugRelatedField(
+        slug_field='uuid',
+        queryset=um.User.objects.all()
+    )
     up_count: s.Field = s.IntegerField(default=0)
     view_count: s.Field = s.IntegerField(default=0)
     created_at: s.Field = s.DateTimeField(read_only=True)
