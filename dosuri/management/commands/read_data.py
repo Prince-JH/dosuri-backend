31d5456d974289cfa4b555bef5fec6ec15b725e3
Prince-JH <alahoon@naver.com>
2022-12-18 20:23:05 +0900

    feat: read hospital, treatment data

--- dosuri/management/commands/read_data.py
+++ dosuri/management/commands/read_data.py
@@ -14,8 +14,8 @@ class Command(BaseCommand):
     help = 'Insert Hospital Data'
 
     def add_arguments(self, parser):
-        parser.add_argument('file-loc')
         parser.add_argument('data-type')
+        parser.add_argument('file-loc')
 
     def handle(self, *args, **options):
         file_loc = options['file-loc']
@@ -31,56 +31,46 @@ def read_hospital_data(file_loc):
     file = open(loc)
     reader = csv.reader(file)
     for line in reader:
-        code = line[0]
-        name = line[1]
-        full_address = line[4]
-        area = line[8]
-        longitude = line[20]
-        latitude = line[21]
+        try:
+            code = line[0]
+            name = line[1]
+            full_address = line[3]
+            large_area = line[5]
+            small_area = line[6]
+            area = line[7]
+            phone_no = line[8]
+            opened_at = datetime.strptime(line[10], '%Y-%m-%d')
+            longitude = line[18]
+            latitude = line[19]
 
-        hospital_qs = hm.Hospital.objects.filter(code=code)
-        if hospital_qs.exists():
-            hospital_qs.update(area=area)
-            print(code, area)
-        else:
-            addresses = full_address.split(' ')
-            do, city, gun, gu = None, None, None, None
-            for address in addresses:
-                if address == '':
-                    continue
-                elif address[-1] == '도':
-                    do = address
-                elif address[-1] == '시':
-                    city = address
-                elif address[-1] == '군':
-                    gun = address
-                elif address[-1] == '구':
-                    gu = address
-            phone_no = line[9]
-            opened_at = datetime.strptime(line[11], '%Y-%m-%d')
-            hospital_obj = hm.Hospital.objects.create(
-                code=code,
-                address=full_address,
-                name=name,
-                phone_no=phone_no,
-                opened_at=opened_at,
-                latitude=latitude,
-                longitude=longitude,
-                area=area
-            )
-            address_qs = cm.Address.objects.filter(do=do, city=city, gun=gun, gu=gu)
-            if not address_qs.exists():
-                address_obj = cm.Address.objects.create(
-                    do=do,
-                    city=city,
-                    gun=gu,
-                    gu=gu
+            # print(code, name, full_address, large_area, small_area, area, phone_no, opened_at, longitude, latitude)
+
+            hospital_qs = hm.Hospital.objects.filter(code=code)
+            if hospital_qs.exists():
+                pass
+            else:
+                hospital_obj = hm.Hospital.objects.create(
+                    code=code,
+                    address=full_address,
+                    name=name,
+                    phone_no=phone_no,
+                    opened_at=opened_at,
+                    latitude=latitude,
+                    longitude=longitude,
+                    area=area
                 )
-            hm.HospitalAddressAssoc.objects.create(
-                hospital=hospital_obj,
-                address=address_obj
-            )
-            print(do, city, gun, gu, phone_no, opened_at, latitude, longitude)
+                address_qs = cm.Address.objects.filter(large_area=large_area, small_area=small_area)
+                if not address_qs.exists():
+                    address_obj = cm.Address.objects.create(
+                        large_area=large_area, small_area=small_area
+                    )
+                hm.HospitalAddressAssoc.objects.create(
+                    hospital=hospital_obj,
+                    address=address_obj
+                )
+                print('INSERT:', code)
+        except:
+            pass
 
 
 def read_hospital_treatment_data(file_loc):
@@ -88,13 +78,12 @@ def read_hospital_treatment_data(file_loc):
     file = open(loc)
     reader = csv.reader(file)
 
-    no_data_list = []
     for line in reader:
-        code = line[1]
-        name = line[5]
-        price = line[6].replace(',', '')
-        description = line[7]
-        price_per_hour = line[8].replace(',', '') or None
+        code = line[0]
+        name = line[4]
+        price = line[5].replace(',', '')
+        description = line[6]
+        price_per_hour = line[7].replace(',', '') or None
         print(code, name, description, price, price_per_hour)
 
         try:
@@ -114,7 +103,4 @@ def read_hospital_treatment_data(file_loc):
                     description=description
                 )
         except hm.Hospital.DoesNotExist:
-            no_data_list.append(code)
-    with open('new file', 'w') as f:
-        for data in no_data_list:
-            f.write(data + '\n')
+            pass
