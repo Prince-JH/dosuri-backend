86b504e8169954dcf110e9d9f65b5cc2a9dd1c73
Prince-JH <alahoon@naver.com>
2022-12-18 17:51:32 +0900

    feat: erase default setting db logger

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'SIGNING_KEY': SECRET_KEY,
-    'ALGORITHM': 'HS256',
-    'AUTH_HEADER_TYPES': 'Bearer'
+    'ALGORITHM': 'HS256'
 }
 
 ROOT_URLCONF = 'dosuri.urls'
@@ -107,31 +106,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    }
-}
 DEBUG = False
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
