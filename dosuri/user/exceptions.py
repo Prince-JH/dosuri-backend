b4a2c655e2f15cac7a934a6a450f26e8f76d8093
Prince-JH <alahoon@naver.com>
2022-12-27 23:28:01 +0900

    feat: UserHasNoAddress exception

--- dosuri/user/exceptions.py
+++ dosuri/user/exceptions.py
@@ -4,4 +4,8 @@ from rest_framework import exceptions as exc
 class NicknameDuplicated(exc.APIException):
     default_detail = 'Nickname duplicated.'
     status_code = 409
-    
+
+
+class UserHasNoAddress(exc.APIException):
+    default_detail = 'User does not have address info.'
+    status_code = 400
