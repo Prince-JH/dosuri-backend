8ee2af0989b4ae4f8f8cc4ea1769805b33b83d1a
SCHeo <tjheo@ajou.ac.kr>
2022-12-28 15:25:33 +0900

    modifiy image upload module lambda

--- image-upload/src/app.py
+++ image-upload/src/app.py
@@ -2,35 +2,58 @@ import boto3
 import json
 import base64
 import hashlib
+import cgi
 from requests_toolbelt.multipart import decoder
+import os
 
 BUCKET_NAME = 'dosuri-image'
+FILE_PATH = 'uploads/'
 
-response = {
+response  = {
     'statusCode': 200,
+    'headers': {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+    },
     'body': ''
 }
 
+WAS_ENDPOINT = os.environ['WAS_SERVER']
+
+s3 = boto3.client('s3')
+
+def string_escape(s, encoding='utf-8'):
+    return (s.encode('latin1')         # To bytes, required by 'unicode-escape'
+             .decode('unicode-escape') # Perform the actual octal-escaping decode
+             .encode('latin1')         # 1:1 mapping back to bytes
+             .decode(encoding))        # Decode original encoding
 
 def lambda_handler(event, context):
-    # s3 = boto3.client('s3')
-    print('body:', event['body'])
-    print('headers:', event['headers'])
-    # res['headers'] = event['headers']
-    # res['body'] = event['body']
-    decode = decoder.MultipartDecoder(event['body'], event['headers']['content-type'])
-    #
-    #
+    content = ''
+    headers = ''
+    
+    body = base64.b64decode(event['body-json']) 
+    
+    if 'content-type' in event['params']['header']:
+        content_type = event['params']['header']['content-type']
+    else:
+        content_type = event['params']['header']['Content-Type']
+    
+    decode = decoder.MultipartDecoder(body,content_type)
+    file_name = ""
     for part in decode.parts:
-        print('part.content:', part.content)
-        print('part.headers:', part.headers)
+        content = part.content
         headers = part.headers
-    #     res['content'].append(content)
-    #     res['headers'].append(headers)
-
-    # try:
-    #     s3_response = s3.put_object(Bucket=BUCKET_NAME, Key=full_name, Body=content)
-    # except Exception as e:
-    #     raise IOError(e)
-    # response['body'] = json.dumps({'url': f'{"s3 image url 경로"}/{file_path}{file_name}.jpg'})
+        value, params = cgi.parse_header(str(part.headers[b'Content-Disposition']))
+        if params['name'] == 'file':
+            file_name = string_escape(params['filename'])
+            data = part.content
+    try:
+        s3_response = s3.put_object(Bucket=BUCKET_NAME, Key=FILE_PATH+file_name, Body=data)
+    except Exception as e:
+        raise IOError(e)
+    response['body'] = {
+        'bucket_name': BUCKET_NAME,
+        'path': f'{FILE_PATH}{file_name}'
+    }
     return response
