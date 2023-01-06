66d20cd3dd7bd65ca582ce0fc7d36b6ce3418476
jihoon <jihoon.kim@makinarocks.ai>
2023-01-06 16:02:13 +0900

    feat: UserAuthListAPIView

---
+++ dosuri/common/generics.py
@@ -0,0 +1,18 @@
+from rest_framework import (
+    generics as g,
+    filters as rf,
+    permissions as p, status
+)
+from rest_framework.response import Response
+
+
+class UserAuthListAPIView(g.ListAPIView):
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset(request.user))
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
