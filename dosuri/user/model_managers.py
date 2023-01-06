07c936ee717bda2ce25f2ba7db333be4139e9d26
jihoon <jihoon.kim@makinarocks.ai>
2023-01-06 17:22:53 +0900

    feat: UserPointHistoryManager, UserNotificationManager

--- dosuri/user/model_managers.py
+++ dosuri/user/model_managers.py
@@ -18,3 +18,32 @@ class DosuriUserManager(UserManager):
 
     def update_user_info(self, user, user_info):
         self.filter(pk=user.pk).update(**user_info)
+
+
+class UserPointHistoryManager(UserManager):
+    def create_history(self, user, point, content):
+        qs = self.filter(user=user)
+        if qs.exists():
+            last_point = qs.order_by('-created_at').first().total_point
+        else:
+            last_point = 0
+        return self.create(
+            user=user,
+            modify_point=point,
+            total_point=last_point + point,
+            content=content
+        )
+
+
+class UserNotificationManager(UserManager):
+    def create_notification(self, user, content):
+        return self.create(
+            user=user,
+            content=content,
+            is_new=True
+        )
+
+    def check_notification(self, uuid):
+        return self.filter(uuid=uuid).update(
+            is_new=False
+        )
