785b767f36998f3d0190efa62327eeca9d2e37b0
Prince-JH <alahoon@naver.com>
2023-01-15 14:15:32 +0900

    feat: update user info

--- dosuri/user/views.py
+++ dosuri/user/views.py
@@ -85,18 +85,21 @@ class UserNickname(g.RetrieveAPIView):
 
 class UserDetail(g.RetrieveUpdateDestroyAPIView):
     permission_classes = [p.IsAuthenticated]
-    queryset = get_user_model().objects.all()
+    queryset = get_user_model().objects.filter()
     serializer_class = s.User
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = request.user
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    def get_object(self):
+        return self.request.user
 
-    def destroy(self, request, *args, **kwargs):
-        instance = request.user
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # def retrieve(self, request, *args, **kwargs):
+    #     instance = request.user
+    #     serializer = self.get_serializer(instance)
+    #     return Response(serializer.data)
+    #
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = request.user
+    #     instance.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class InsuranceUserAssocList(g.CreateAPIView):
