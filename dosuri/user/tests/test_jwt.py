3affafc6982755c26b599b43b5abd3b7a0a5d4b1
Prince-JH <alahoon@naver.com>
2023-01-16 22:39:05 +0900

    feat: test get token and validate

--- dosuri/user/tests/test_jwt.py
+++ dosuri/user/tests/test_jwt.py
@@ -4,29 +4,29 @@ import json
 
 class TestToken:
 
-    # @pytest.mark.django_db
-    # def test_get_token_with_valid_data(self, client, dummy_user):
-    #     data = {
-    #         "username": dummy_user.username
-    #     }
-    #     response = client.post('/user/v1/token', data)
-    #     content = json.loads(response.content)
-    #     assert response.status_code == 200
-    #     assert content['access']
-    #     assert content['refresh']
-    #
-    #
-    # @pytest.mark.django_db
-    # def test_verify_valid_token(self, client, dummy_user):
-    #     data = {
-    #         "username": dummy_user.username
-    #     }
-    #     response = client.post('/user/v1/token', data)
-    #     content = json.loads(response.content)
-    #     assert response.status_code == 200
+    @pytest.mark.django_db
+    def test_get_token_with_valid_data(self, client, user_dummy):
+        data = {
+            "username": user_dummy.username
+        }
+        response = client.post('/user/v1/users/token', data)
+        content = json.loads(response.content)
+        assert response.status_code == 200
+        assert content['access']
+        assert content['refresh']
 
-        # response = client.post('/user/v1/token/verify', {'token': content['access']})
-        # assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_verify_valid_token(self, client, user_dummy):
+        data = {
+            "username": user_dummy.username
+        }
+        response = client.post('/user/v1/users/token', data)
+        content = json.loads(response.content)
+        assert response.status_code == 200
+
+        response = client.post('/user/v1/token/verify', {'token': content['access']})
+        assert response.status_code == 200
 
     @pytest.mark.django_db
     def test_verify_invalid_token(self, client):
