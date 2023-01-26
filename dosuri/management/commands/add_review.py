63cccf637a298e123f5b7ecc911b3b691ac07209
SCHeo <tjheo@ajou.ac.kr>
2023-01-26 18:25:58 +0900

    modified add review

--- dosuri/management/commands/add_review.py
+++ dosuri/management/commands/add_review.py
@@ -18,29 +18,37 @@ def add_article(df_list):
     df_list=df_list['df']
     article_list = []
     for idx, row in df_list.iterrows():
-        count=count+1
-        print(str(pid)+" progress: "+ str((count/data_len)*100) + "%")
-        user=um.User.objects.get(username=row['리뷰어이름'])
-        if m.Article.objects.filter(content=row['리뷰내용']).exists():
-            continue
         try:
-            hospital_id = hm.Hospital.objects.get(code=row['코드']).id
+            count=count+1
+            print(str(pid)+" progress: "+ str((count/data_len)*100) + "%")
+            user=um.User.objects.get(username=row['리뷰어이름'])
+            try:
+                hospital_id = hm.Hospital.objects.get(code=row['코드']).id
+            except:
+                hospital_id = None
+            org_created_at = row['날짜'][0:-4].replace("\n", "")
+            created_at = datetime.strptime(org_created_at, "%Y년 %m월 %d일")
+            article_list.append(m.Article(
+                user = user,
+                status = "complete",
+                article_type = "review",
+                hospital_id = hospital_id,
+                content = row['리뷰내용'],
+                created_at = created_at
+            ))
         except:
-            hospital_id = None
-        org_created_at = row['날짜'][0:-4].replace("\n", "")
-        created_at = datetime.strptime(org_created_at, "%Y년 %m월 %d일")
-        article_list.append(m.Article(
-            user = user,
-            status = "complete",
-            article_type = "review",
-            hospital_id = hospital_id,
-            content = row['리뷰내용'],
-            created_at = created_at
-        ))
+            article_list.append(m.Article(
+                user_id = 1,
+                status = "complete",
+                article_type = "review",
+                content = row['리뷰내용'],
+                created_at = created_at
+            ))
     m.Article.objects.bulk_create(article_list, batch_size=1000)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        m.Article.objects.filter(article_type='review').delete()
         df = pd.read_csv('review.csv')
         df_list=[]
         for i in range(0,47):
