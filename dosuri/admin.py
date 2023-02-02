f59ca73253c8b02b2d8701519a6206bd0219609d
Prince-JH <alahoon@naver.com>
2023-02-02 23:41:54 +0900

    feat: Article, ArticleAuth

--- dosuri/admin.py
+++ dosuri/admin.py
@@ -16,4 +16,18 @@ admin.site.register(hm.Doctor)
 admin.site.register(hm.DoctorDescription)
 admin.site.register(hm.DoctorKeywordAssoc)
 
+
+class ArticleAuthAdminInline(admin.TabularInline):
+    model = cmm.ArticleAuth
+    extra = 0
+    exclude = ('uuid',)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleAuthAdminInline]
+
+
+admin.site.register(cmm.Article, ArticleAdmin)
+admin.site.register(cmm.ArticleAuth)
+
 admin.site.register(um.User)
