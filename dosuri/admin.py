00f4a03b732e432e223c77419391dfcde4572dc1
Prince-JH <alahoon@naver.com>
2023-02-04 19:41:04 +0900

    feat: improve ArticleAdmin spped

--- dosuri/admin.py
+++ dosuri/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from dosuri.common import models as cm
 from dosuri.community import models as cmm
@@ -22,9 +23,22 @@ class ArticleAuthAdminInline(admin.TabularInline):
     extra = 0
     exclude = ('uuid',)
 
+#
+# class RelationForm(forms.ModelForm):
+#     parent = forms.ChoiceField(required=False,
+#                                choices=Relation.objects.values_list('id', 'name'))
+#     particle = forms.ChoiceField(required=False,
+#                                  choices=Particle.objects.values_list('id', 'content'))
+#     media = forms.ChoiceField(required=False,
+#                               choices=Media.objects.values_list('id', 'name'))
+#
+#     class Meta:
+#         model = Relation
+
 
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [ArticleAuthAdminInline]
+    raw_id_fields = ['hospital', 'user']
     list_display = ['content', 'user', 'created_at']
     search_fields = ['content']
 
