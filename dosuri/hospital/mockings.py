fa63573dbee56fb4b96b9ce0d19f941f907d8fdf
Prince-JH <alahoon@naver.com>
2023-02-06 22:34:53 +0900

    feat: geoapi mock data

---
+++ dosuri/hospital/mockings.py
@@ -0,0 +1,61 @@
+수원시_팔달구_coordinates = {
+    "documents": [
+        {
+            "address": {
+                "address_name": "경기 수원시 팔달구",
+                "b_code": "4111500000",
+                "h_code": "4111500000",
+                "main_address_no": "",
+                "mountain_yn": "N",
+                "region_1depth_name": "경기",
+                "region_2depth_name": "수원시 팔달구",
+                "region_3depth_h_name": "",
+                "region_3depth_name": "",
+                "sub_address_no": "",
+                "x": "127.020135072307",
+                "y": "37.2826740965805"
+            },
+            "address_name": "경기 수원시 팔달구",
+            "address_type": "REGION",
+            "road_address": None,
+            "x": "127.020135072307",
+            "y": "37.2826740965805"
+        }
+    ],
+    "meta": {
+        "is_end": True,
+        "pageable_count": 1,
+        "total_count": 1
+    }
+}
+
+서울특별시_강남구_coordinates = {
+    "documents": [
+        {
+            "address": {
+                "address_name": "서울 강남구",
+                "b_code": "1168000000",
+                "h_code": "1168000000",
+                "main_address_no": "",
+                "mountain_yn": "N",
+                "region_1depth_name": "서울",
+                "region_2depth_name": "강남구",
+                "region_3depth_h_name": "",
+                "region_3depth_name": "",
+                "sub_address_no": "",
+                "x": "127.047377408384",
+                "y": "37.517331925853"
+            },
+            "address_name": "서울 강남구",
+            "address_type": "REGION",
+            "road_address": None,
+            "x": "127.047377408384",
+            "y": "37.517331925853"
+        }
+    ],
+    "meta": {
+        "is_end": True,
+        "pageable_count": 1,
+        "total_count": 1
+    }
+}
