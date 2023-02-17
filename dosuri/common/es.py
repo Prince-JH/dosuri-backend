e44d73f04237c0835b657e889ad0c7421a0b130c
SCHeo <tjheo@ajou.ac.kr>
2023-02-17 17:00:20 +0900

    예외처리 추가...

--- dosuri/common/es.py
+++ dosuri/common/es.py
@@ -19,32 +19,59 @@ def es_middleware(get_response):
             request_data=json.loads(request.body.decode())
         else:
             request_data={}
-        response = get_response(request)
-        finished_time=time.time()
-        execution_time=finished_time - start_time
-        # print("REQUEST:")
-        api_failed=False
+        
         if request.user.is_authenticated:
             username=request.user.username
         else:
             username='anomymous'
+            
+        api_failed=False
         if response.status_code < 200 or response.status_code >= 400:
             api_failed=True
+        
+        if request.GET:
+            request_params=dict(request.GET)
+        else:
+            request_params={}
+        
+        if request.headers:
+            request_headers=dict(request.headers)
+        else:
+            request_headers={}
+
+        response = get_response(request)
+
+        if response.headers:
+            response_headers=dict(response.headers)
+        else:
+            response_headers={}
+        
+        if response.data:
+            response_data = response.data
+        else:
+            response_data = {}
+
+        finished_time=time.time()
+        execution_time=finished_time - start_time
         data={
             'HTTP_method': request.method,
             'status_code': response.status_code,
             'is_failed': api_failed,
             'request_body': request_data,
-            'request_params': dict(request.GET),
-            'request_headers': dict(request.headers),
+            'request_params': request_params,
+            'request_headers': request_headers,
             'username': username,
             'path': request.path,
-            'response_headers': dict(response.headers),
-            'response_data': response.data,
+            'response_headers': response_headers,
+            'response_data': response_data,
             "@timestamp" : datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z',
             'execution_time': "{:.6f}".format(execution_time),
         }
-        es.index(index="api_call_index", body=data)
+        try:
+            es.index(index="api_call_index", body=data)
+        except:
+            import traceback
+            traceback.print_exc()
 
         return response
 
