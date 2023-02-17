01b9664e3ac953cd522eb8ad1f73a770688f7de2
SCHeo <tjheo@ajou.ac.kr>
2023-02-17 16:30:00 +0900

    add es in prod

--- dosuri/common/es.py
+++ dosuri/common/es.py
@@ -1,14 +1,51 @@
-from elasticsearch import Elasticsearch
 from django.conf import settings
+import time
+import json
+import datetime
+from django.utils.dateparse import parse_datetime
+from rest_framework.fields import DateTimeField
+from django.conf import settings
+from elasticsearch import Elasticsearch
 
-cloud_id = 'dosuri:YXAtbm9ydGhlYXN0LTIuYXdzLmVsYXN0aWMtY2xvdWQuY29tOjQ0MyQ4OWFmZjJhYmVlMTY0YjUxODIxZDQ0N2ExYTdmMGJjNiQzZTFiYzA1YTQxYjM0MWE1YWYyMjhlZTBhMzEzODJiOA=='
-user = 'elastic'
-pw = 'w6JzEhNIcV6zUySl0pM6M78Y'
+es = Elasticsearch(settings.ES_ENDPOINT+':9200', http_auth=(settings.ES_USERNAME, settings.ES_PASSWORD))
 
-client = Elasticsearch(
-    cloud_id=settings.cloud_id,
-    http_auth=(user, pw)
-)
-client.search(
-    index="test-index"
-)
+# custom middlewawre - factory method pattern
+
+def es_middleware(get_response):
+
+    def middleware(request):
+        start_time=time.time()
+        if request.body:
+            request_data=json.loads(request.body.decode())
+        else:
+            request_data={}
+        response = get_response(request)
+        finished_time=time.time()
+        execution_time=finished_time - start_time
+        # print("REQUEST:")
+        api_failed=False
+        if request.user.is_authenticated:
+            username=request.user.username
+        else:
+            username='anomymous'
+        if response.status_code < 200 or response.status_code >= 400:
+            api_failed=True
+        data={
+            'HTTP_method': request.method,
+            'status_code': response.status_code,
+            'is_failed': api_failed,
+            'request_body': request_data,
+            'request_params': dict(request.GET),
+            'request_headers': dict(request.headers),
+            'username': username,
+            'path': request.path,
+            'response_headers': dict(response.headers),
+            'response_data': response.data,
+            "@timestamp" : datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z',
+            'execution_time': "{:.6f}".format(execution_time),
+        }
+        es.index(index="api_call_index", doc_type="_doc", body=data)
+
+        return response
+
+    return middleware
