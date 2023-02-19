e0919c6040053cafb8498db713c2f1b9e09d5f3b
Prince-JH <alahoon@naver.com>
2023-02-20 00:36:00 +0900

    feat: modify celery sh

--- .ebextensions/celery-worker.sh
+++ .ebextensions/celery-worker.sh
@@ -1,54 +1,27 @@
 #!/usr/bin/env bash
 
-# Get django environment variables
-celeryenv=`cat /opt/python/current/env | tr '\n' ',' | sed 's/export //g' | sed 's/$PATH/%(ENV_PATH)s/g' | sed 's/$PYTHONPATH//g' | sed 's/$LD_LIBRARY_PATH//g'`
-celeryenv=${celeryenv%?}
+# Create the celery systemd service file
+echo "[Unit]
+Name=Celery
+Description=Celery service for My App
+After=network.target
+StartLimitInterval=0
 
-# Create celery configuraiton script
-celeryconf="[program:celeryd-worker]
-; Set full path to celery program if using virtualenv
-command=/opt/python/run/venv/bin/celery worker -A PROJECT_NAME -P solo --loglevel=INFO -n worker.%%h
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+WorkingDirectory=/var/app/current
+ExecStart=$PYTHONPATH/celery worker -A documerge --loglevel=INFO -n worker.%%h
+EnvironmentFile=/opt/elasticbeanstalk/deployment/env
 
-directory=/opt/python/current/app/enq_web
-user=nobody
-numprocs=1
-stdout_logfile=/var/log/celery/worker.log
-stderr_logfile=/var/log/celery/worker.log
-autostart=true
-autorestart=true
-startsecs=10
+[Install]
+WantedBy=multi-user.target
+" | tee /etc/systemd/system/celery.service
 
-; Need to wait for currently executing tasks to finish at shutdown.
-; Increase this if you have very long running tasks.
-stopwaitsecs = 600
+# Start celery service
+systemctl start celery.service
 
-; When resorting to send SIGKILL to the program to terminate it
-; send SIGKILL to its whole process group instead,
-; taking care of its children as well.
-killasgroup=true
-
-; if rabbitmq is supervised, set its priority higher
-; so it starts first
-priority=998
-
-environment=$celeryenv
-"
-
-# Create the celery supervisord conf script
-echo "$celeryconf" | tee /opt/python/etc/celery.conf
-
-# Add configuration script to supervisord conf (if not there already)
-if ! grep -Fxq "[include]" /opt/python/etc/supervisord.conf
-  then
-  echo "[include]" | tee -a /opt/python/etc/supervisord.conf
-  echo "files: celery.conf" | tee -a /opt/python/etc/supervisord.conf
-fi
-
-# Reread the supervisord config
-/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf reread
-
-# Update supervisord in cache without restarting all services
-/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf update
-
-# Start/Restart celeryd through supervisord
-/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart celeryd-worker
+# Enable celery service to load on system start
+systemctl enable celery.service`
