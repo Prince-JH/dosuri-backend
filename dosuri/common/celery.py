40f9c4ff3533fa6a1a1aa1151a6482e9c12b0bfc
jihoon <jihoon.kim@makinarocks.ai>
2023-02-19 17:24:29 +0900

    feat: celery

---
+++ dosuri/common/celery.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings_local')
+app = Celery('dosuri')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
