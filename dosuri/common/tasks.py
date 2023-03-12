261e20064fa9fc8831857e7f8ee0f7cb40a7fef3
Prince-JH <alahoon@naver.com>
2023-03-12 15:15:01 +0900

    feat: modify message to insurance requester

--- dosuri/common/tasks.py
+++ dosuri/common/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from dosuri.common import utils as cu
 from dosuri.common import sms as cs
+from dosuri.common import slack as csl
 from django.conf import settings
 
 
@@ -8,12 +9,14 @@ from django.conf import settings
 def announce_insurance_consult(message):
     naver_client = cs.NaverCloudClient()
     naver_client.send_sms(message, settings.INSURANCE_PHONE_NUMBERS)
-    cu.send_slack(message)
+    slack_client = csl.SlackClient()
+    slack_client.send_message(message)
 
 
 @shared_task
 def announce_insurance_consult_to_user(phone_no):
-    message = '도수치료 리얼후기 도수리입니다. \n' \
+    phone_no = phone_no.replace('-', '')
+    message = '[도수리] \n' \
               '실손보험 담당자가 곧 연락드릴 예정입니다. \n' \
               '곽혜경 010-9071-4800'
     naver_client = cs.NaverCloudClient()
