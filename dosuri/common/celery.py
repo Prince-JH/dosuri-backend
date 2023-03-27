415fc9c8a3cab5cf81c6d8b4970f50aaa838c00d
SCHeo <tjheo@ajou.ac.kr>
2023-03-27 12:29:56 +0900

    modified celery

--- dosuri/common/celery.py
+++ dosuri/common/celery.py
@@ -10,13 +10,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings_local')
 app = Celery('dosuri')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.conf.update(
-    CELERY_TASK_SERIALIZER='json',
-    CELERY_ACCEPT_CONTENT=['json'],
-    CELERY_RESULT_SERIALIZER='json',
-    CELERY_TIMEZONE='Asia/Seoul',
-    CELERY_ENABLE_UTC=False,
-    CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler',
-)
-
+app.conf.beat_schedule = {
+    'article_relocation_every_day': {  
+        'task': 'dosuri.tasks.article_relocation_every_day',   
+        'schedule': crontab(hour=23, minute=59),      
+        'args': () 
+    },
+    'test_batch_every_min': {  
+        'task': 'dosuri.tasks.test_batch_every_min',   
+        'schedule': crontab(minute='*'), 
+        'args': ()       
+    },
+}
 app.autodiscover_tasks()
