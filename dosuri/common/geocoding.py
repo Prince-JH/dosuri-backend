c3aa101e7d7d51cbfd64ef66f40814a500fc127a
Prince-JH <alahoon@naver.com>
2023-04-01 00:20:57 +0900

    fix: query_public_institutions_by_address

--- dosuri/common/geocoding.py
+++ dosuri/common/geocoding.py
@@ -32,7 +32,7 @@ class KaKaoGeoClient:
 
     def query_public_institutions_by_address(self, address):
         parsed_address = parse.quote(address)
-        url = f'https://dapi.kakao.com/v2/local/search/keyword.json?query={parsed_address}&category_group_code=PO9'
+        url = f'https://dapi.kakao.com/v2/local/search/keyword.json?query={parsed_address}&category_group_code=PO3'
         response = requests.get(url, headers=self.set_api_header())
         if response.status_code != 200:
             raise exc.APIException()
