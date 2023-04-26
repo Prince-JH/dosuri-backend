76fc070082e63508872cfb1ba75c5d0d7949ffdc
jihoon <jihoon.kim@makinarocks.ai>
2023-04-26 22:49:11 +0900

    fix: UserAddressList filters my address only

--- dosuri/user/views.py
+++ dosuri/user/views.py
@@ -176,6 +176,19 @@ class UserAddressList(g.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_queryset(self, user):
+        return self.queryset.filter(user=user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset(request.user))
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class UserAddressDetail(g.RetrieveUpdateDestroyAPIView):
     permission_classes = [p.IsAuthenticated]
