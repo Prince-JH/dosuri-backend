ea02c7c1e7546358360c0dc26859dc5eb55adea3
Prince-JH <alahoon@naver.com>
2023-07-03 23:31:25 +0900

    feat: HospitalContactPoint

--- dosuri/hospital/models.py
+++ dosuri/hospital/models.py
@@ -224,3 +224,22 @@ class HospitalReservation(models.Model):
     class Meta:
         db_table = 'hospital_reservation'
         ordering = ['-id']
+
+
+class HospitalContactPoint(models.Model):
+    uuid = models.CharField(max_length=32, default=generate_uuid, db_index=True)
+    TYPE_CHOICES = (
+        ('representative', '대표 전화번호'),
+        ('counseling', '상담 관리자 번호'),
+        ('event', '이벤트 관리자 번호'),
+        ('advertisement', '마케팅 담당자 이메일'),
+    )
+
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, related_name='hospital_contact_point')
+    contact_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    contact_point = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'hospital_contact_point'
+        ordering = ['-id']
